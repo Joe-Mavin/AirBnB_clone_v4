747274ec2bb6182d634b31207cb4b825c875529b
Joe-mavin <mavinodundo@gmail.com>
2024-01-14 13:57:13 +0300

    changes to forked

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -10,10 +11,9 @@ from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Representation of a user"""
-
-    if models.storage_t == "db":
-        __tablename__ = "users"
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -29,4 +29,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        self.password = md5(self.password.encode()).hexdigest()
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
